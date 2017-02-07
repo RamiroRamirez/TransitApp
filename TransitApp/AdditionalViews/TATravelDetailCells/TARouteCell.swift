a5448446357aad0c2ad3e4ea85f38679b333e333
Ramiro Ramirez <ramiro.ramirez@smartmobilefactory.com>
2017-02-07 21:11:20 +0100

    Adding comments/documentation

--- TransitApp/AdditionalViews/TATravelDetailCells/TARouteCell.swift
+++ TransitApp/AdditionalViews/TATravelDetailCells/TARouteCell.swift
@@ -19,10 +19,17 @@ class TARouteCell									: UITableViewCell {
 	// MARK: - Setup cell
 
 	func setupCell(segment: Segment) {
+
+		// if there is segement name, use it for the transport name title
+		// if not, use only the travel mode
 		let nameText = ((segment.name != nil) ? "\(segment.travelMode) \(segment.name!)" : nil)
 		self.transportNameTitle?.text = (nameText ?? segment.travelMode)
+
+		// show the number of stops, if there is one
 		let numberOfStops: String? = ((segment.numberStops != 0) ? "\(segment.numberStops) stops" : nil)
 		self.detailsTitle?.text = numberOfStops
+
+		// set the color of the segment/route
 		self.routeLineView?.backgroundColor = UIColor(fromHexString: segment.color)
 	}
 }
